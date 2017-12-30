20f1735ed072ee60f76ab8035ac302214218fc87
Ulises García <neuville_11@hotmail.com>
2017-12-29 18:26:31 -0600

    precompile assets

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module TicketManager
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.time_zone = "America/Mexico_City"
-    config.assets.unknown_asset_fallback = true
+    config.assets.precompile += ['tickets.scss']
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
